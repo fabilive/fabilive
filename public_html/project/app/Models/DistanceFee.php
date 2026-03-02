9be2d385a2be6f34e089e2ed12b2794433b12e1e
DigitalExpart <214820464+DigitalExpart@users.noreply.github.com>
2026-03-02 14:28:05 +0100

    Split: Frontend branch - removed backend logic and core folders

--- public_html/project/app/Models/DistanceFee.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class DistanceFee extends Model
-{
-    protected $fillable = ['distance_start_range','distance_end_range','fee'];
-}
