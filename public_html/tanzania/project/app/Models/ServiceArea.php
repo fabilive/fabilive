b46c16d46f5fb1c30cecefeb04c5771ea95e8403
DigitalExpart <214820464+DigitalExpart@users.noreply.github.com>
2026-03-02 14:06:12 +0100

    Split: Backend branch - removed frontend assets and tanzania folder

--- public_html/tanzania/project/app/Models/ServiceArea.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class ServiceArea extends Model
-{
-    protected $fillable = ['location'];
-    public $timestamps = false;
-}
