f5260faa510bf65df9341f3c84751c5fe0ee3db3
DigitalExpart <214820464+DigitalExpart@users.noreply.github.com>
2026-04-09 12:41:09 +0100

    Fix homepage crash: handle missing product slugs in blade partials

--- resources/views/partials/product/best-product.blade.php
+++ resources/views/partials/product/best-product.blade.php
@@ -7,14 +7,14 @@
                     <model-viewer class="product-3d-viewer" src="{{ asset($prod->{'3d_model'}) }}" ar ar-modes="webxr scene-viewer quick-look" camera-controls touch-action="pan-y" shadow-intensity="1" auto-rotate></model-viewer>
                 </div>
                 @else
-                <a href="{{ route('front.product', $prod->slug) }}" class="woocommerce-LoopProduct-link"><img src="{{ $prod->thumbnail ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt="Product Image"></a>
+                <a href="{{ route('front.product', $prod->slug ?: 'missing-slug') }}" class="woocommerce-LoopProduct-link"><img src="{{ $prod->thumbnail ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt="Product Image"></a>
                 @endif
                 @if (round($prod->offPercentage() )>0)
                 <div class="on-sale">-{{ round($prod->offPercentage() )}}%</div>
                 @endif
             </div>
             <div class="product-info">
-                <h3 class="product-title"><a href="{{ route('front.product', $prod->slug) }}">{{ $prod->showName() }}</a></h3>
+                <h3 class="product-title"><a href="{{ route('front.product', $prod->slug ?: 'missing-slug') }}">{{ $prod->showName() }}</a></h3>
                 <div class="product-price">
                     <div class="price">
                         <ins>{{ $prod->showPrice() }} </ins>
