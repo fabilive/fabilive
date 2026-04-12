3d63c04f9c7a333e1948bc02f81c80c3fb9fd9a2
DigitalExpart <214820464+DigitalExpart@users.noreply.github.com>
2026-04-12 17:09:56 +0100

    Fix 405 error by initializing payment on load and further simplify campay input

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -1506,6 +1506,9 @@ $('.payment').on('click', function () {
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
     $(document).ready(function() {
+        // Automatically select the first payment method to initialize the form action
+        $('.payment').first().click();
+
         $('#service_area_id').select2({
             placeholder: 'Select Service Area',
             allowClear: true,
